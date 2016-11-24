6968ebcc7b3097ce14564790aa9ae1c1baf03d1f
rmzse <rodrigo@therawdelicious.com>
2016-11-24 20:09:38 +0100

    clean up old object code

--- spec/fb_engine_spec.js
+++ spec/fb_engine_spec.js
@@ -1,15 +1,9 @@
 describe('FizzBuzz Logic', function() {
   var number;
-  // var test;
 
   describe('happy path: all numbers are divided with no remainder', function() {
-    // beforeEach(function() {
-    //   fizzbuzz = new FizzBuzz({number: 45});
-    // });
-
 
     it('accepts a number and checks for its division by 15', function() {
-      // expect(fizzbuzz.test15(number)).toEqual(true);
       number = 45;
       expect(FizzBuzz().test15(number)).toEqual(true);  // call it #fizzbuzz() later
     });
--- src/fb_engine.js
+++ src/fb_engine.js
@@ -2,8 +2,4 @@ function FizzBuzz() {
 
   function test15(number) { return true; }
 
-  // FizzBuzz.prototype.test15 = function () {
-  //   fizzbuzz = new FizzBuzz();
-  //   return true;
-  // };
 }
