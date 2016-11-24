0f6f3fb961753bb46fb39b0af1485d2150479a43
rmzse <rodrigo@therawdelicious.com>
2016-11-24 20:00:04 +0100

    add simpler test functino

---
+++ src/fb_engine.js
@@ -0,0 +1,9 @@
+function FizzBuzz() {
+
+  function test15(number) { return true;}
+
+  // FizzBuzz.prototype.test15 = function () {
+  //   fizzbuzz = new FizzBuzz();
+  //   return true;
+  // };
+}
