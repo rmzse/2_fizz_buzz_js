7d307226832b69b889ee39e6666ac90e7c4a2ebd
rmzse <rodrigo@therawdelicious.com>
2016-11-24 20:50:05 +0100

    simplify the code and forget about making private fnctions for fizzbuzz, fizz and buzz

--- spec/fb_engine_spec.js
+++ spec/fb_engine_spec.js
@@ -4,4 +4,12 @@ describe('FizzBuzz Logic', function() {
       expect(test(45)).toEqual(true);  // call it #fizzbuzz() later
     });
 
+    it('accepts a number and checks for its division by 15', function() {
+      expect(test(45)).toEqual(true);  // call it #fizzbuzz() later
+    });
+
+    it('accepts a number and checks for its division by 15', function() {
+      expect(test(45)).toEqual(true);  // call it #fizzbuzz() later
+    });
+
 });
