fac79a6df20021cae3b42ebb44b5281dcacb5f34
rmzse <rodrigo@therawdelicious.com>
2016-11-24 22:13:05 +0100

    push it again

--- spec/ui_spec.js
+++ spec/ui_spec.js
@@ -5,7 +5,7 @@ describe('FizzBuzz UI', function(){
     $.holdReady(false);
   });
 
-  it('accepts the number 45 and returns \"fizzbuzz\"', function(){
+  it('accepts the number 45 and displays \"fizzbuzz\"', function(){
     $('#number').val('45');
     $('#btn').click();
     expect($('#message').text()).toEqual('fizzbuzz');
