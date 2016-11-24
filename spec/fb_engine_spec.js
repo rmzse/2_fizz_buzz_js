b8c0caebef3ac0375c4c2521d81738045d46685e
rmzse <rodrigo@therawdelicious.com>
2016-11-24 20:25:14 +0100

    simplify simplify

--- spec/fb_engine_spec.js
+++ spec/fb_engine_spec.js
@@ -1,10 +1,7 @@
 describe('FizzBuzz Logic', function() {
 
-  describe('happy path: all numbers are divided with no remainder', function() {
-
     it('accepts a number and checks for its division by 15', function() {
       expect(test(45)).toEqual(true);  // call it #fizzbuzz() later
     });
-    
-  });
+
 });
