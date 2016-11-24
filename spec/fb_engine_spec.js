b96aa6adc04831ed28cc09870d7811c94a4568d8
rmzse <rodrigo@therawdelicious.com>
2016-11-24 20:21:54 +0100

    add missing files

--- spec/fb_engine_spec.js
+++ spec/fb_engine_spec.js
@@ -1,14 +1,10 @@
 describe('FizzBuzz Logic', function() {
-  var number;
 
   describe('happy path: all numbers are divided with no remainder', function() {
 
     it('accepts a number and checks for its division by 15', function() {
-      number = 45;
-      expect(test(number)).toEqual(true);  // call it #fizzbuzz() later
+      expect(test(45)).toEqual(true);  // call it #fizzbuzz() later
     });
-
-
-
+    
   });
 });
