7fca0f24a27ac096e05e6449c722af318b532b12
rmzse <rodrigo@therawdelicious.com>
2016-11-24 22:04:11 +0100

    include ui spec to repo2

---
+++ spec/ui_spec.js
@@ -0,0 +1,13 @@
+describe('FizzBuzz UI', function(){
+  beforeEach(function() {
+    jasmine.getFixtures().fixturesPath = 'base/spec/fixtures';
+    loadFixtures('sample_partial.html');
+    $.holdReady(false);
+  });
+
+  it('accepts the number 45 and returns \"fizzbuzz\"', function(){
+    $('#number').val('45');
+    $('#btn').click();
+    expect($('#message').text()).toEqual('fizzbuzz');
+  });
+});
