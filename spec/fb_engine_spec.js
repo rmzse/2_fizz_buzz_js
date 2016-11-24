b1f4aedf8186ad59d0c88c20db514fe81c81666b
rmzse <rodrigo@therawdelicious.com>
2016-11-24 19:56:23 +0100

    add a number to test with

--- spec/fb_engine_spec.js
+++ spec/fb_engine_spec.js
@@ -10,7 +10,8 @@ describe('FizzBuzz Logic', function() {
 
     it('accepts a number and checks for its division by 15', function() {
       // expect(fizzbuzz.test15(number)).toEqual(true);
-      expect(test15(number)).toEqual(true);
+      number = 45;
+      expect(test15(number)).toEqual(true);  // call it #fizzbuzz() later
     });
 
 
